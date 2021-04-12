1453df7e735e9621048e4dd575b86ed8d388018f
joanpadolina <34737850+joanpadolina@users.noreply.github.com>
2021-04-12 09:54:51 +0200

    Rebase calculation from header to page

--- components/header/header-main/header-main.js
+++ components/header/header-main/header-main.js
@@ -1,28 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
-import { useSelector } from 'react-redux';
 
-export default function Header({props}) {
-  const [currentCarbon, setCurrentCarbon] = useState(0);
-  const [trees, setTrees] = useState();
-  const userCarbon = useSelector((state) => state.user);
-  const user = userCarbon.userData;
-
-  useEffect(() => {
-    const chargeCarbonTotal = userCarbon.historyCharge.reduce(
-      (sum, { savedCarbon }) => Math.ceil(sum + savedCarbon),
-      0,
-    );
-    setCurrentCarbon(chargeCarbonTotal);
-
-    function calcTreeSavings() {
-      const averageTree = 20;
-      const totalTrees = Math.floor(currentCarbon / averageTree);
-      return setTrees(totalTrees);
-    }
-
-    calcTreeSavings();
-  }, [userCarbon.historyCharge, userCarbon]);
+export default function Header({user, props}) {
 
   return (
     <header className="header">
@@ -37,12 +16,12 @@ export default function Header({props}) {
         <section>
           <span className="header__carbon-subtitle">Your total saved CO2</span>
           <h2 className="header__carbon-amount font--big">
-            {currentCarbon} kg
+            {props.currentCarbon} kg
           </h2>
         </section>
       </Link>
       <p className="header__body">
-        Your total CO2 saving is the same amount as {trees} trees absorbs CO2 in
+        Your total CO2 saving is the same amount as {props.trees} trees absorbs CO2 in
         1 year.
       </p>
     </header>
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,37 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { fetchUser } from '../redux/actions';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { carbonReducer } from '../lib/carbon-saving-calculation';
 import HeaderCarbon from '../components/header/header-main/header-main';
 import SavingsTime from '../components/savings-time';
 import CommunityUpdate from '../components/community-update/community-update';
 
-export default function Home() {
+export default function Index() {
+  const userCarbon = useSelector((state) => state.user);
+  const [currentCarbon, setCurrentCarbon] = useState(0);
+  const [trees, setTrees] = useState(0);
+  const user = userCarbon.userData;
+
+  useEffect(() => {
+    const chargeCarbonTotal = carbonReducer(userCarbon.historyCharge);
+    setCurrentCarbon(chargeCarbonTotal);
+
+    function calcTreeSavings() {
+      const averageTree = 20;
+      const totalTrees = Math.floor(currentCarbon / averageTree);
+      return setTrees(totalTrees);
+    }
+
+    calcTreeSavings();
+  }, [userCarbon.historyCharge, userCarbon, currentCarbon]);
+
+  const data = {
+    currentCarbon,
+    trees,
+  };
+
   return (
     <div className="home__body">
-      <HeaderCarbon />
+      <HeaderCarbon props={data} user={user} />
       <main>
         <SavingsTime />
         <CommunityUpdate />
