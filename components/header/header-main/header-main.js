74bd747389ba390f0916f415406c57cb2be2911b
joanpadolina <34737850+joanpadolina@users.noreply.github.com>
2021-04-20 14:04:54 +0200

    Icon base added recent charge

--- components/header/header-main/header-main.js
+++ components/header/header-main/header-main.js
@@ -19,21 +19,22 @@ export default function Header({ user, currentCarbon }) {
       <div>
         <h1 className="header__intro">Hello {user.name},</h1>
         <div className="header__account"></div>
+        <span className="header__subtitle">This is your savings since 01-01-2021</span>
       </div>
       <Link
         className="header__carbon flex flex-col justify-center items-center h-2/4"
         href="/profile"
       >
         <section className="header__savings">
-          <span className='header__carbon-subtitle'>Your total saved CO2</span>
-          <h2 className='header__carbon-amount font--big'>
+          <span className="header__carbon-subtitle">Your total saved CO2</span>
+          <h2 className="header__carbon-amount font--big">
             {currentCarbon} kg
           </h2>
         </section>
       </Link>
       <p className="header__body">
-        Your total CO2 saving is the same amount as {trees} trees absorbs
-        CO2 in 1 year.
+        Your total CO2 saving is the same amount as {trees} trees absorbs CO2 in
+        1 year.
       </p>
     </header>
   );
