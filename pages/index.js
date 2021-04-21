e83787fd293734d232f739017cbff4b8725ed939
joanpadolina <34737850+joanpadolina@users.noreply.github.com>
2021-04-21 19:35:00 +0200

    Added progress in index

--- pages/index.js
+++ pages/index.js
@@ -6,23 +6,28 @@ import HeaderCarbon from '../components/header/header-main/header-main';
 import SavingsTime from '../components/savings-time';
 import CommunityUpdate from '../components/community-update/community-update';
 import HistoryCharge from '../components/history-charge/history-charge';
-import PopupAmount from '../components/popup-amount';
 
 export default function Index() {
-  const user = useSelector((state) => state.store.user);
+  const store = useSelector((state) => state.store)
+  const user = store.user;
+  const community = store.community
+  
   const [currentCarbon, setCurrentCarbon] = useState(0);
-  const store = useSelector((state) => state.store);
+  const [communityCarbon, setCommunityCarbon] = useState(0);
+  const [total, setTotal] = useState(0)
   const { chargingSession } = store;
 
   useEffect(() => {
     const chargeCarbonTotal = carbonReducer(chargingSession);
+    const communityTotalCarbon = carbonReducer(community.users)
+    setCommunityCarbon(communityTotalCarbon);
     setCurrentCarbon(chargeCarbonTotal);
+    setTotal(currentCarbon + communityCarbon);
   }, [chargingSession, currentCarbon]);
 
   return (
     <div className="home__body">
       <HeaderCarbon user={user} currentCarbon={currentCarbon} />
-      {/* <PopupAmount /> */}
       <main className="index__main">
         <SavingsTime />
         <HistoryCharge />
