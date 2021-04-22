7dce6b9d3bb7006b0f48e5c2902fd7ed6f86f5f6
joanpadolina <34737850+joanpadolina@users.noreply.github.com>
2021-04-22 16:57:10 +0200

    Profile added tree styling and progress bar

--- components/progress-bar/progress-bar.js
+++ components/progress-bar/progress-bar.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 export default function ProgressBar({
   totalSavingsCommunity,
   totalCommunityGoal,
+  noShow
 }) {
   const [currentProgress, setCurrentProgress] = useState('');
 
--- pages/profile.js
+++ pages/profile.js
@@ -1,19 +1,21 @@
-import React, { useEffect, useState } from "react";
-import Link from "next/link";
-import { useSelector } from "react-redux";
-import { carbonReducer } from "../lib/carbon-saving-calculation";
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { useSelector } from 'react-redux';
+import { carbonReducer } from '../lib/carbon-saving-calculation';
+import ProgressBar from '../components/progress-bar';
 
 export default function Profile() {
   const [totalSavings, setTotalSavings] = useState(0);
   const [totalTrees, setTotalTrees] = useState(0);
   const [arrayOfTrees, setArrayOfTrees] = useState([]);
+  const [gimmicksInKm, setGimmicksInKm] = useState({
+    plane: 200,
+    car: 130,
+    train: 300,
+  });
+
   const store = useSelector((state) => state.store);
   const { chargingSession } = store;
-
-  // tree effect
-  // 1. seperate in layers
-  // 2. max per row is 9 trees
-  // 3. perspective layers is unlimited?
 
   useEffect(() => {
     calcTreeSavings();
@@ -47,20 +49,22 @@ export default function Profile() {
 
   return (
     <>
-      <header>
-        <Link href='/'>back</Link>
+      <header className="profile__header">
+        <Link href="/">
+          <a className="button--back" aria-label="Go to homepage"></a>
+        </Link>
         <h1>Your CO2 savings </h1>
-        <span>{totalSavings}</span>
+        <h2 className="font--big">{totalSavings} kg</h2>
       </header>
-      <main className='profile__main'>
+      <main className="profile__main index__main">
         <article>
           <h2>Your garden so far</h2>
           <p>Amount of trees saved in one year</p>
-          <section>
+          <section className="profile__trees-wrapper">
             <p>An average tree absorbs around 20kg of CO2 in one year.</p>
-            <section className='profile__trees'>
+            <section className="profile__trees">
               {arrayOfTrees.map((tree, index) => (
-                <img key={index} className='profile__tree' src={tree} />
+                <img key={index} className="profile__tree" src={tree} />
               ))}
             </section>
           </section>
@@ -73,23 +77,40 @@ export default function Profile() {
               <tbody>
                 <tr>
                   <td>plane</td>
-                  <td className='profile__equivalence-bar'>balk</td>
-                  <td>
-                    <span>26 km</span> <span>195 kg/km</span>
+                  <td className="profile__equivalence-bar">
+                    <ProgressBar
+                      totalSavingsCommunity={150}
+                      totalCommunityGoal={300}
+                    />
+                  </td>
+                  <td className="profile__equivalence-body">
+                    {gimmicksInKm.plane} km <span>195 kg/km</span>{' '}
                   </td>
                 </tr>
                 <tr>
                   <td>car</td>
-                  <td className='profile__equivalence-bar'>balk</td>
-                  <td>
-                    <span>45 km</span> <span>121 kg/km</span>
+                  <td className="profile__equivalence-bar">
+                    {' '}
+                    <ProgressBar
+                      totalSavingsCommunity={250}
+                      totalCommunityGoal={300}
+                    />
+                  </td>
+                  <td className="profile__equivalence-body">
+                    {gimmicksInKm.car} km <span>121 kg/km</span>{' '}
                   </td>
                 </tr>
                 <tr>
                   <td>train</td>
-                  <td className='profile__equivalence-bar'>balk</td>
-                  <td>
-                    <span>200 km</span> <span>30 kg/km</span>
+                  <td className="profile__equivalence-bar">
+                    {' '}
+                    <ProgressBar
+                      totalSavingsCommunity={300}
+                      totalCommunityGoal={300}
+                    />
+                  </td>
+                  <td className="profile__equivalence-body">
+                    {gimmicksInKm.train} km <span>30 kg/km </span>
                   </td>
                 </tr>
               </tbody>
