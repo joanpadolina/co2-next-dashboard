6f80eaba6933e2a5fc819d753cb79221c0559ee7
joanpadolina <34737850+joanpadolina@users.noreply.github.com>
2021-04-23 13:10:48 +0200

    Fixed bar chart on profile

--- pages/profile.js
+++ pages/profile.js
@@ -9,10 +9,35 @@ export default function Profile() {
   const [totalTrees, setTotalTrees] = useState(0);
   const [arrayOfTrees, setArrayOfTrees] = useState([]);
   const [gimmicksInKm, setGimmicksInKm] = useState({
-    plane: 200,
-    car: 130,
-    train: 300,
+    plane: 0,
+    car: 0,
+    train: 0,
   });
+  const [widthStyle, setWidthStyle] = useState({
+    plane: 0,
+    car: 0,
+    train: 0,
+  });
+
+  const carbonPerKm = {
+    plane: 195,
+    car: 121,
+    train: 30,
+  };
+
+  function calculateDistance(carbon, gimmick) {
+    const resultDistance = (carbon * 100) / gimmick;
+    return Math.ceil(resultDistance);
+  }
+  // Needs for calc
+  // --> All carbon emission from gimmick
+  // --> Calculate user emission in kilometers
+
+  // Steps to km barchart
+  // 1. Calculate carbon with all values from gimmicks
+  // 2. Find the highest number
+  // 3. Calculate remainder gimmicks in % from the highest number
+  // 4. Place % it in width
 
   const store = useSelector((state) => state.store);
   const { chargingSession } = store;
@@ -28,6 +53,32 @@ export default function Profile() {
     }
     setArrayOfTrees(arraySrc);
     addImgTrees(totalTrees);
+
+    let object;
+
+    function setObjectGimmick() {
+      const value = {
+        plane: calculateDistance(totalSavings, carbonPerKm.plane),
+        car: calculateDistance(totalSavings, carbonPerKm.car),
+        train: calculateDistance(totalSavings, carbonPerKm.train),
+      };
+      setGimmicksInKm(value);
+    }
+
+    setObjectGimmick();
+
+    function calcPercentageBar() {
+      const values = Object.values(gimmicksInKm);
+      const max = Math.max(...values);
+      const calc = (number) => (100 * number) / max;
+      const value = {
+        plane: calc(gimmicksInKm.plane),
+        car: calc(gimmicksInKm.car),
+        train: 65,
+      };
+      setWidthStyle(value);
+    }
+    calcPercentageBar();
   }, [totalTrees, totalSavings, chargingSession]);
 
   function calcTreeSavings() {
@@ -56,12 +107,15 @@ export default function Profile() {
         <h1>Your CO2 savings </h1>
         <h2 className="font--big">{totalSavings} kg</h2>
       </header>
+
       <main className="profile__main index__main">
         <article>
-          <h2>Your garden so far</h2>
+          <h2 className="font--title">Your garden so far</h2>
           <p>Amount of trees saved in one year</p>
+
           <section className="profile__trees-wrapper">
             <p>An average tree absorbs around 20kg of CO2 in one year.</p>
+
             <section className="profile__trees">
               {arrayOfTrees.map((tree, index) => (
                 <img key={index} className="profile__tree" src={tree} />
@@ -69,46 +123,62 @@ export default function Profile() {
             </section>
           </section>
         </article>
+
         <article>
-          <h2>Your total savings equivalence</h2>
+          <h2 className="font--title">Your total savings equivalence</h2>
           <span> in kilometer </span>
+
           <section>
-            <table>
-              <tbody>
-                <tr>
-                  <td>plane</td>
-                  <td className="profile__equivalence-bar">
-                    <ProgressBar
+            <table className="profile__equivalence">
+              <tbody className="profile__equivalence-wrapper">
+                <tr className="profile__equivalence-row">
+                  <td className="profile__equivalence-title">plane</td>
+                  <td
+                    className="profile__equivalence-bar"
+                    style={{ width: `${widthStyle.plane}%` }}
+                  >
+                    {/* <ProgressBar
                       totalSavingsCommunity={150}
                       totalCommunityGoal={300}
-                    />
+                    /> */}
                   </td>
+
                   <td className="profile__equivalence-body">
                     {gimmicksInKm.plane} km <span>195 kg/km</span>{' '}
                   </td>
                 </tr>
-                <tr>
-                  <td>car</td>
-                  <td className="profile__equivalence-bar">
+
+                <tr className="profile__equivalence-row">
+                  <td className="profile__equivalence-title">car</td>
+                  <td
+                    className="profile__equivalence-bar"
+                    style={{ width: `${widthStyle.car}%` }}
+                  >
                     {' '}
-                    <ProgressBar
+                    {/* <ProgressBar
                       totalSavingsCommunity={250}
                       totalCommunityGoal={300}
-                    />
+                    /> */}
                   </td>
+
                   <td className="profile__equivalence-body">
                     {gimmicksInKm.car} km <span>121 kg/km</span>{' '}
                   </td>
                 </tr>
-                <tr>
-                  <td>train</td>
-                  <td className="profile__equivalence-bar">
+
+                <tr className="profile__equivalence-row">
+                  <td className="profile__equivalence-title">train</td>
+                  <td
+                    className="profile__equivalence-bar"
+                    style={{ width: `${widthStyle.train}%` }}
+                  >
                     {' '}
-                    <ProgressBar
+                    {/* <ProgressBar
                       totalSavingsCommunity={300}
                       totalCommunityGoal={300}
-                    />
+                    /> */}
                   </td>
+
                   <td className="profile__equivalence-body">
                     {gimmicksInKm.train} km <span>30 kg/km </span>
                   </td>
