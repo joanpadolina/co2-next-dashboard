434ae3583ca21f37c7888f005a01d36b75cd6857
joanpadolina <34737850+joanpadolina@users.noreply.github.com>
2021-04-26 12:51:58 +0200

    Header styled

--- pages/charge-input.js
+++ pages/charge-input.js
@@ -1,45 +1,43 @@
-import React, { useState, useEffect } from "react";
-import { useRouter } from "next/router";
+import React, { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 import {
   carbonSavingCalculation,
-  carbonReduces,
-} from "../lib/carbon-saving-calculation";
-import diff from "../lib/time-calculation";
-import { useSelector, useDispatch } from "react-redux";
-import { addCharge, addTotal } from "../redux/actions";
+  carbonReduces
+} from '../lib/carbon-saving-calculation'
+import diff from '../lib/time-calculation'
+import { useSelector, useDispatch } from 'react-redux'
+import { addCharge, addTotal } from '../redux/actions'
 
 export default function ChargeTime({ show, onClose }) {
-  const [isBrowser, setIsBrowser] = useState(false);
-  const router = useRouter();
-  const [carbon, setCarbon] = useState(0);
-  const [currentDate, setCurrentDate] = useState("");
-  const userCarbon = useSelector((state) => state.user);
-  const userHistory = userCarbon.historyCharge;
-  const { userData } = userCarbon;
-  
-  const dispatch = useDispatch();
+  const [isBrowser, setIsBrowser] = useState(false)
+  const router = useRouter()
+  const [carbon, setCarbon] = useState(0)
+  const [currentDate, setCurrentDate] = useState('')
+  const userCarbon = useSelector((state) => state.user)
+  const userHistory = userCarbon.historyCharge
+  const { userData } = userCarbon
+
+  const dispatch = useDispatch()
 
   useEffect(() => {
-    if (router.asPath === "/charge-input") {
-      setIsBrowser(!isBrowser);
+    if (router.asPath === '/charge-input') {
+      setIsBrowser(!isBrowser)
     }
-    setIsBrowser(true);
-
+    setIsBrowser(true)
 
     try {
-      const chargeCarbonTotal = carbonReducer(userHistory);
-      dispatch(addTotal(carbon));
-      setCarbon(chargeCarbonTotal);
+      const chargeCarbonTotal = carbonReducer(userHistory)
+      dispatch(addTotal(carbon))
+      setCarbon(chargeCarbonTotal)
     } catch (err) {
-      console.error(err);
+      console.error(err)
     }
 
     // set date to today
-    const setDateToday = new Date().toISOString().substr(0, 10);
-    setCurrentDate(setDateToday);
-
-  }, [carbon, dispatch, userHistory, setCurrentDate]);
+    const setDateToday = new Date().toISOString().substr(0, 10)
+    setCurrentDate(setDateToday)
+  }, [carbon, dispatch, userHistory, setCurrentDate])
 
   function chargeConfig(config = {}) {
     const newCharge = {
@@ -48,141 +46,141 @@ export default function ChargeTime({ show, onClose }) {
       end: config.endTime,
       duration: config.duration,
       savingsInPercentage: config.savings(),
-      savedCarbon: config.savedCarbon(),
-    };
-    return newCharge;
+      savedCarbon: config.savedCarbon()
+    }
+    return newCharge
   }
 
   function handleSubmit(e) {
-    e.preventDefault();
-    const input = handleInput(e);
-    const data = chargeConfig(input);
-    
-    dispatch(addCharge(data));
+    e.preventDefault()
+    const input = handleInput(e)
+    const data = chargeConfig(input)
+
+    dispatch(addCharge(data))
   }
 
   function handleInput(e) {
-    const date = e.target[0].value;
-    const startTime = e.target[1].value;
-    const endTime = e.target[2].value;
-    const duration = diff(startTime, endTime);
-    
-    const savedCarbon = () => {
-      const total = carbonSavingCalculation(duration, userData);
-      return total / 100;
-    };
+    const date = e.target[0].value
+    const startTime = e.target[1].value
+    const endTime = e.target[2].value
+    const duration = diff(startTime, endTime)
 
-    const savings = () => Math.floor(Math.random() * 20) + "%";
+    const savedCarbon = () => {
+      const total = carbonSavingCalculation(duration, userData)
+      return total / 100
+    }
+
+    const savings = () => Math.floor(Math.random() * 20) + '%'
     return {
       date,
       startTime,
       endTime,
       duration,
       savedCarbon,
-      savings,
-    };
+      savings
+    }
   }
 
   function changeDate(e) {
-    return setCurrentDate(e.target.value);
+    return setCurrentDate(e.target.value)
   }
 
   return (
-    <section className='charge-input charge-input--modal'>
-      <h2 className='charge-input__title font--title'>
+    <section className="charge-input charge-input--modal">
+      <h2 className="charge-input__title font--title">
         At what time did you charge?
       </h2>
       <form
-        className='charge-input__form charge-input__form--modal'
-        action=''
+        className="charge-input__form charge-input__form--modal"
+        action=""
         onSubmit={(e) => handleSubmit(e)}
       >
         <label
-          className='charge-input__label a11y-sr-only'
-          aria-label='date'
-          htmlFor='date'
+          className="charge-input__label a11y-sr-only"
+          aria-label="date"
+          htmlFor="date"
         >
           date
         </label>
         <input
-          className='charge-input__date'
-          id='date'
-          type='date'
-          value={currentDate || ""}
+          className="charge-input__date"
+          id="date"
+          type="date"
+          value={currentDate || ''}
           onChange={(e) => changeDate(e)}
         ></input>
-        <div className='charge-input__amount-wrapper'>
-          <label className='charge-input__label' htmlFor='start'>
+        <div className="charge-input__amount-wrapper">
+          <label className="charge-input__label" htmlFor="start">
             start
             <input
-              className='charge-input__time charge-input__border--default'
-              id='start'
-              type='time'
-              min='00:00'
-              max='23:59'
+              className="charge-input__time charge-input__border--default"
+              id="start"
+              type="time"
+              min="00:00"
+              max="23:59"
               required
             ></input>
           </label>
-          <label className='charge-input__label' htmlFor='end'>
+          <label className="charge-input__label" htmlFor="end">
             end
             <input
-              className='charge-input__time charge-input__border--default'
-              id='end'
-              type='time'
-              min='00:00'
-              max='23:59'
+              className="charge-input__time charge-input__border--default"
+              id="end"
+              type="time"
+              min="00:00"
+              max="23:59"
               required
             ></input>
           </label>
         </div>
 
-        <div className='charge-input__value-wrapper'>
-          <label for='charge-value' className='charge-input__charge-label'>
+        <div className="charge-input__value-wrapper">
+          <label for="charge-value" className="charge-input__charge-label">
             Charging amount
           </label>
           <input
-            id='charge-value'
-            type='number'
-            className='charge-input__amount charge-input__border--default'
+            id="charge-value"
+            type="number"
+            className="charge-input__amount charge-input__border--default"
             required
           ></input>
 
-          <div className='charge-input__toggle'>
+          <div className="charge-input__toggle">
             <input
-              type='radio'
-              className='charge-input__radio'
-              id='km'
-              name='amount'
+              type="radio"
+              className="charge-input__radio"
+              id="km"
+              name="amount"
               defaultChecked
             />
             <label
-              for='km'
-              className='charge-input__radio-label'
-              aria-label='kilometers'
+              for="km"
+              className="charge-input__radio-label"
+              aria-label="kilometers"
             >
               km
             </label>
 
             <input
-              type='radio'
-              id='kWh'
-              className='charge-input__radio'
-              name='amount'
+              type="radio"
+              id="kWh"
+              className="charge-input__radio"
+              name="amount"
             />
             <label
-              for='kWh'
-              className='charge-input__radio-label'
-              aria-label='kilowatt-hour'
+              for="kWh"
+              className="charge-input__radio-label"
+              aria-label="kilowatt-hour"
             >
               kWh
             </label>
           </div>
         </div>
-        <div className='button-wrapper'>
-          <button className='button'>save changes</button>
+        <div className="button-wrapper">
+          <button className="button">save changes</button>
         </div>
       </form>
-      <a className='button-flat button--secondary'> add another session</a>
+      <a className="button-flat button--secondary"> add another session</a>
     </section>
-  );
+  )
 }
--- pages/community.js
+++ pages/community.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import Link from 'next/link'
 import { useSelector, useDispatch } from 'react-redux'
 import { communitySavings } from '../redux/actions/'
 import { members } from '../lib/members'
@@ -27,7 +26,7 @@ export default function Community() {
       setCarbonInKm(petrolDistance)
     }
     calcInKm()
-    console.log(totalCarbon)
+
     const historyLast = chargingSession
       .slice(-1)
       .map((data) => data.savedCarbon)
@@ -55,100 +54,106 @@ export default function Community() {
   ])
 
   return (
-    <main>
-      <Link href='/'>back</Link>
-      <h1>Your community</h1>
-      <article>
-        <section className='community-saved__datavisual'>
-          <article>
-            <h2>Community total saved CO2</h2>
-            <p>{totalCarbon} kg</p>
-          </article>
-          <BubbleComparison />
-          <article>
-            <h2>Your contribution</h2>
-            <p>
-              {userContribution}
-              kg
-            </p>
-          </article>
-        </section>
+    <>
+      <header className='header'>
+        <h1>Your community</h1>
+        <article className='community__header-body'>
+          <p className='community__header-p community__header-people community__header-padding--right'>
+            <span className='community__header-amount'> 8 </span> People
+          </p>
+          <p className='community__header-p community__header-padding--left'>
+            <span className='community__header-amount'> 3 </span> Destination
+            reached
+          </p>
+        </article>
+      </header>
+      <main className='index__main'>
+        <article>
+          <section className='community-saved__datavisual'>
+            <article>
+              <h2 className='font--title'>Community total saved CO2</h2>
+              <BubbleComparison />
+              <p>
+                The amount of CO2 saved is the same as driving{' '}
+                <span>{carbonInKm} km</span> in a petrol car.
+              </p>
+            </article>
+          </section>
+        </article>
 
-        <section>
-          <article>
-            <h3>amsterdam</h3>
-            <h3>rotterdam</h3>
-          </article>
-          <article>
-            <p>
-              The amount of CO2 saved is the same as driving from Amsterdam to
-              Maastricht in a petrol car,{' '}
-              <span className='font-bold'> which is 212,5 km </span> .
-            </p>
-          </article>
-        </section>
-      </article>
-      <article>
-        <h2>Next destination</h2>
-        <section>
-          <h3>Amsterdam -- Luxembourg</h3>
-
+        <article>
+          <h2 className='font--title'>Communities next goal</h2>
+          <section>
+            <div>
+              <h3>Amsterdam {'-->'} Luxembourg</h3>
+              <p>{carbonInKm} km</p>
+            </div>
+            <div>
+              <p>
+                500 kg
+                <span>~410 km</span>
+              </p>
+            </div>
+          </section>
           <ProgressBar
             totalSavingsCommunity={totalCarbon}
             totalCommunityGoal={500}
           />
-
-          <h3>{carbonInKm}km</h3>
-        </section>
-      </article>
-
-      <article>
-        <h3>Activiy</h3>
-        <table>
-          <tbody>
-            <tr>
-              <th></th>
-              <th>Name</th>
-              <th>CO2 saved</th>
-            </tr>
-
-            {membersContribution.map((user, key) => (
-              <tr key={key}>
-                <td>
-                  {members.map((data) => {
-                    if (data.name.includes(user.name)) {
-                      return (
-                        <img
-                          alt={user.name}
-                          key={key}
-                          src={data.name.includes(user.name) ? data.imgSrc : ''}
-                        />
-                      )
-                    }
-                    return ''
-                  })}
-                </td>
-                <td> {user.name}</td>
-                <td>{user.savedCarbon}</td>
+        </article>
+        <article>
+          <h2 className='font--title'> Co2 contributions</h2>
+          <p> -- chart -- </p>
+        </article>
+        <article>
+          <h2 className='font--title'>Activiy</h2>
+          <table>
+            <tbody>
+              <tr>
+                <th></th>
+                <th>Name</th>
+                <th>CO2 saved</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </article>
 
-      <article>
-        <h3>Community members</h3>
-        <ul>
-          {members.map((member, key) => (
-            <li key={key}>
-              <figure>
-                <img src={member.imgSrc} alt={member.name} />
-                <figcaption>{member.name}</figcaption>
-              </figure>
-            </li>
-          ))}
-        </ul>
-      </article>
-    </main>
+              {membersContribution.map((user, key) => (
+                <tr key={key}>
+                  <td>
+                    {members.map((data) => {
+                      if (data.name.includes(user.name)) {
+                        return (
+                          <img
+                            alt={user.name}
+                            key={key}
+                            src={
+                              data.name.includes(user.name) ? data.imgSrc : ''
+                            }
+                          />
+                        )
+                      }
+                      return ''
+                    })}
+                  </td>
+                  <td> {user.name}</td>
+                  <td>{user.savedCarbon}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </article>
+
+        <article>
+          <h3>Community members</h3>
+          <ul>
+            {members.map((member, key) => (
+              <li key={key}>
+                <figure>
+                  <img src={member.imgSrc} alt={member.name} />
+                  <figcaption>{member.name}</figcaption>
+                </figure>
+              </li>
+            ))}
+          </ul>
+        </article>
+      </main>
+    </>
   )
 }
