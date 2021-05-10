e6a1bcca23ad90ccb970f9cc67dfb8b1e7c57477
joanpadolina <34737850+joanpadolina@users.noreply.github.com>
2021-05-10 12:49:15 +0200

    Header without circle

--- components/header/header-main/header-main.js
+++ components/header/header-main/header-main.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
+import IconSoftArrow from '../../../public/icons/icon-soft-arrow.svg'
 
 export default function Header({ user, currentCarbon }) {
   const [trees, setTrees] = useState(0)
@@ -19,29 +20,25 @@ export default function Header({ user, currentCarbon }) {
       <div>
         <h1 className='header__intro'>Hello {user.name},</h1>
         <div className='header__account'></div>
-        <span className='header__subtitle'>
-          This is your savings since 01-01-2021
-        </span>
       </div>
-      <Link
-        className='header__carbon flex flex-col justify-center items-center h-2/4'
-        href='/profile'
-      >
-        <section className='header__savings'>
-          <span className='header__carbon-subtitle'>Your total saved CO2</span>
-          <h2 className='header__carbon-amount font--big'>
-            {currentCarbon} kg
-          </h2>
-        </section>
-      </Link>
-      <Link
-        className='header__carbon flex flex-col justify-center items-center h-2/4'
-        href='/profile'
-      >
-        <p className='header__body'>
-          Your total CO2 saving is the same amount as {trees} trees absorbs CO2
-          in 1 year.
+      <section className='header__savings'>
+        <p className='header__carbon-subtitle'>
+          Your total saved CO2{' '}
+          <span className='header__subtitle'>since 01-01-2021</span>
         </p>
+        <h2 className='header__carbon-amount'>{currentCarbon} kg</h2>
+      </section>
+      <Link
+        className='header__carbon flex flex-col justify-center items-center h-2/4'
+        href='/profile'
+      >
+        <a className='header__detail-savings'>
+          <p className='header__body'>
+            Your total CO2 saving is the same amount as {trees} trees absorbs
+            CO2 in 1 year.
+            <IconSoftArrow className='header__arrow' />
+          </p>
+        </a>
       </Link>
     </header>
   )
