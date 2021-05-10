7dc12d81f823ba028a308f6ee3873e1cac9dba13
joanpadolina <34737850+joanpadolina@users.noreply.github.com>
2021-05-10 15:38:30 +0200

    Refactored toggle in overview

--- components/emission-overview/emission-overview.js
+++ components/emission-overview/emission-overview.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import IconArrow from '../../public/icons/icon-arrow-up.svg'
 
 export default function EmissionOverview() {
@@ -12,17 +12,13 @@ export default function EmissionOverview() {
     fossil: 85
   }
 
-  function handleToggle(e) {
-    const value = e.target.computedName
-
-    if (value === 'Yearly') {
+  useEffect(() => {
+    if (toggleActive === 'Yearly') {
       setCurrentDate('2021')
     } else {
       setCurrentDate('March 2021')
     }
-
-    setToggleActive(value)
-  }
+  }, [currentDate, toggleActive])
 
   const MonthlyOverview = () => {
     return (
@@ -83,26 +79,38 @@ export default function EmissionOverview() {
     <article className='emission-overview'>
       <h2 className='font--title'>CO2 overview</h2>
       <section className='emission-overview__toggle-wrapper'>
-        <button
-          onClick={(e) => handleToggle(e)}
-          className={` ${
-            toggleActive === 'Monthly'
-              ? 'emission-overview__toggle--enabled '
-              : ''
-          }emission-overview__toggle`}
+        <input
+          type='radio'
+          className='emission-overview__radio'
+          id='monthly'
+          name='kind'
+          value='Monthly'
+          defaultChecked
+          onClick={() => setToggleActive('Monthly')}
+        />
+        <label
+          htmlFor='monthly'
+          className='emission-overview__radio-toggle'
+          aria-label='Monthly'
         >
           Monthly
-        </button>
-        <button
-          onClick={(e) => handleToggle(e)}
-          className={`${
-            toggleActive === 'Yearly'
-              ? 'emission-overview__toggle--enabled'
-              : ''
-          }  emission-overview__toggle`}
+        </label>
+
+        <input
+          type='radio'
+          id='yearly'
+          className='emission-overview__radio'
+          name='kind'
+          value='Yearly'
+          onClick={() => setToggleActive('Yearly')}
+        />
+        <label
+          htmlFor='yearly'
+          className='emission-overview__radio-toggle'
+          aria-label='Yearly'
         >
           Yearly
-        </button>
+        </label>
       </section>
 
       <section className='emission-overview__calendar-selection'>
