30135c57676d2c1bbc04a5de0896cff980df7b6a
joanpadolina <34737850+joanpadolina@users.noreply.github.com>
2021-05-18 12:42:29 +0200

    Styled bar chart timeslots

--- pages/forecast.js
+++ pages/forecast.js
@@ -1,37 +1,110 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import Link from 'next/link'
+import getForecast from '../lib/getForcast'
+import { Bar } from 'react-chartjs-2'
+import { InfluxDB } from '@influxdata/influxdb-client'
 
 export default function Forecast({ forecast }) {
+  const [savings, setSavings] = useState()
+  const [date, setDate] = useState()
+
   useEffect(() => {
-    console.log(forecast)
+    const data = getForecast(forecast)
+    const sorting = data.sort((a, b) => a.label[0] - b.label[0])
+    setSavings(sorting)
   }, [forecast])
 
+  useEffect(() => {
+    const timeElapsed = Date.now()
+    const today = new Date(timeElapsed)
+    setDate(today.toDateString())
+  }, [])
+
+  const savingsData = savings && savings.map((item) => item.savings)
+  const labels = savings && savings.map((item) => item.label)
+
+  const barColor = []
+
+  if (savings) {
+    for (let i = 0; i < savingsData.length; i++) {
+      const green = '#adfdd7'
+      const midgreen = '#48c98c'
+      const low = '#2d7e58'
+
+      if (savingsData[i] >= 15) {
+        barColor[i] = green
+      } else if (savingsData[i] >= 10 || savingsData[i] >= 14) {
+        barColor[i] = midgreen
+      } else {
+        barColor[i] = low
+      }
+    }
+  }
+
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        axis: 'y',
+        label: 'Savings',
+        data: savingsData,
+        backgroundColor: barColor,
+        borderWidth: 1
+      }
+    ]
+  }
+
   return (
-    <div className='savings-time'>
+    <div className='forecast'>
       <header>
         <Link href='/'>
-          <a className='button--back'>back</a>
+          <a className='button--back' aria-label='go back to feed'></a>
         </Link>
         <h1> CO2-Smart Charging:</h1>
-        <h2>Forecast 14 Mei, NL</h2>
+        <h2>Forecast: {date}, NL</h2>
       </header>
       <main>
-        <p>
-          <pre>{JSON.stringify(forecast, null, 2)}</pre>
-        </p>
-        {/* <ul>
-          {forecast.map((item, index) => (
-            <li key={item.index}>{item._value}</li>
-          ))}
-        </ul> */}
+        <Bar
+          data={data}
+          options={{
+            plugins: {
+              legend: {
+                display: false
+              }
+            },
+            indexAxis: 'y',
+            scales: {
+              x: {
+                grid: {
+                  display: false
+                },
+                title: {
+                  display: true,
+                  text: 'savings (%)',
+                  color: '#9E9AA7'
+                }
+              },
+              y: {
+                grid: {
+                  display: false
+                },
+                title: {
+                  display: true,
+                  text: 'timeslots',
+                  color: '#9E9AA7'
+                }
+              }
+            }
+          }}
+          width={400}
+          height={700}
+        />
       </main>
     </div>
   )
 }
 
 export async function getStaticProps() {
-  const { InfluxDB } = require('@influxdata/influxdb-client')
-
   const token = process.env.INFLUXDB_TOKEN
   const url = process.env.INFLUX_API_URL
   const org = process.env.INFLUX_API_ORG
@@ -41,12 +114,12 @@ export async function getStaticProps() {
 
   const queryApi = client.getQueryApi(org)
   const fluxQuery = `from(bucket: "elmap")
-  |> range(start: now(), stop: 2d)
-  |> filter(fn: (r) => r["_measurement"] == "forecast")
-  |> filter(fn: (r) => r["_field"] == "carbonIntensity")
-  |> filter(fn: (r) => r["kind"] == "totals")
-  |> filter(fn: (r) => r["timeoffset"] == "baseline")
-  |> filter(fn: (r) => r["zone"] == "${zone}")`
+    |> range(start: now(), stop: 2d)
+    |> filter(fn: (r) => r["_measurement"] == "forecast")
+    |> filter(fn: (r) => r["_field"] == "carbonIntensity")
+    |> filter(fn: (r) => r["kind"] == "totals")
+    |> filter(fn: (r) => r["timeoffset"] == "baseline")
+    |> filter(fn: (r) => r["zone"] == "${zone}")`
   const data = await queryApi.collectRows(fluxQuery)
 
   return {
